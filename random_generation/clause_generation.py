24d5dcaef3b8751281ee894077a284be079ab71e
Partha Ghosh <pghosh@brown.is.localnet>
2024-03-22 17:56:46 +0100

    Generating random set of clauses. Non exhaustive yet

---
+++ random_generation/clause_generation.py
@@ -0,0 +1,100 @@
+import random
+import string
+from generate_random_proofs import load_definitions_and_rules
+
+
+class ClauseGenerator:
+    def __init__(self, defs):
+        self.defs = defs
+        self.defined_points = []
+        # self.clause_relations = list(defs.keys()) # this is the full set we can't deal with it yet
+        self.clause_relations = ['angle_bisector', 'angle_mirror']
+
+    def generate_point(self):
+        """
+        Generate a random point
+        """
+        # points are 1 or two character names e.g. 'a', 'ab', 'A', 'AB' or a1
+        # Define the character pools
+        letters = string.ascii_letters  # a-z, A-Z
+        digits = string.digits  # 0-9
+        all_chars = letters + digits  # Pool for the second character
+
+        # Start with a letter
+        result = random.choice(letters)
+
+        # Optionally add a second character, which can be a letter or a digit
+        if random.choice([True, False]):  # Decide randomly whether to add a second character
+            result += random.choice(all_chars)
+
+        return result
+
+    def generate_new_point(self):
+        while True:
+            point = self.generate_point()
+            if point not in self.defined_points:
+                return point
+
+    def choose_random_n_defined_points(self, n):
+        """
+        Choose n random defined points
+        """
+        return random.sample(self.defined_points, n)
+
+    def get_text_clause(self, clause_relation, arg_vars, result_vars):
+        """
+        Make a canonical clause for a given relation
+        """
+        if result_vars:
+            clause_txt = f'{" ".join(result_vars)} = {clause_relation} {" ".join(result_vars + arg_vars)}'
+        else:
+            clause_txt = f'{clause_relation} {" ".join(arg_vars)}'
+        return clause_txt
+
+    def choose_random_defined_points(self):
+        if not self.defined_points:  # Check if the list is empty
+            return []  # Return an empty list or handle the scenario as needed
+
+        # Choose a random number of points to select, from 1 up to the length of the list
+        n = random.randint(1, len(self.defined_points))
+
+        # Randomly select 'n' points from the list
+        chosen_defined_pts = random.sample(self.defined_points, n)
+
+        return chosen_defined_pts
+
+    def generate_clauses(self, n):
+        """
+        Generate n random clauses with all points defined
+        """
+        clauses = []
+        for i in range(n):
+            # choose a random definition key as the first clause
+            clause_relation = random.choice(self.clause_relations)
+            needs_defined_points = len(self.defs[clause_relation].args)
+
+            chosen_defined_pts = self.choose_random_defined_points()
+            num_pts_to_def = needs_defined_points - len(chosen_defined_pts)
+            # Generate names of points that are needed for the clause
+            will_be_defined_pts = []
+            while num_pts_to_def > 0:
+                will_be_defined_pts.append(self.generate_new_point())
+                self.defined_points.append(will_be_defined_pts[-1])
+                num_pts_to_def -= 1
+
+            clause = self.get_text_clause(clause_relation, chosen_defined_pts, will_be_defined_pts)
+            clauses.append(clause)
+
+        return '; '.join(clauses)
+
+
+if __name__ == "__main__":
+    defs_path = '../defs.txt'
+    rules_path = '../rules.txt'
+
+    # Load definitions and rules
+    definitions, rules = load_definitions_and_rules(defs_path, rules_path)
+    cg = ClauseGenerator(definitions)
+    print(cg.get_text_clause('angle_bisector', ['x', 'y'], ['b', 'c']))
+
+    print(cg.generate_clauses(3))
--- random_generation/generate_random_proofs.py
+++ random_generation/generate_random_proofs.py
@@ -24,21 +24,20 @@ def main():
     # Load definitions and rules
     definitions, rules = load_definitions_and_rules(defs_path, rules_path)
 
-    # txt = 'a b c = triangle a b c; x = circle x a b c ? perp a b c x'
-    # txt = 'a b c = triangle a b c; x = circle x a b c ? triangle a b c'
-    txt = 'a b c = triangle a b c; d = midpoint d a b; e = midpoint e b c; f = midpoint f c a; g = on_line g d c, on_line g e a ? coll f b g'
-    # txt = 'a b c = triangle a b c; d = midpoint d a b; e = midpoint e b c; f = midpoint f c a; g = on_line g d c, on_line g e a'
-    # txt = 'A P B = cong A P B P; Q = cong A Q B Q, cyclic A B P Q ? perp P A A Q'
-    # txt = 'a b c = triangle a b c; d = on_tline d b a c, on_tline d c a b; e = on_line e a c, on_line e b d ? perp a d b c'  # pylint: disable=line-too-long
+    txt = 'a b c = triangle a b c; x = circle x a b c ? perp a b c x'
+    # txt = 'a b c = triangle a b c; d = midpoint d a b; e = midpoint e b c; f = midpoint f c a; g = on_line g d c, on_line g e a ? coll f b g'
+
+    # A goal less starting point of the search
+    txt = 'a b c = triangle a b c; d = midpoint d a b; e = midpoint e b c; f = midpoint f c a; g = on_line g d c, on_line g e a'
     p = pr.Problem.from_txt(txt)
     g, _ = gh.Graph.build_problem(p, definitions)
-    # deductions, _, _, _ = saturate_or_goal(g, rules, [], p)
-    #
+
     ddar.solve(g, rules, p, max_level=1000)
+
+    # Randomly select a cache node to be the goal. #TODO: Is this right can we do better? Consider coverage!
     cache_node = random.choice(list((g.cache.keys())))
     goal = pr.Construction(cache_node[0], list(cache_node[1:]))
     write_solution(g, p, goal=goal, out_file='')
-
 
 
 if __name__ == "__main__":
