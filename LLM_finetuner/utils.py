fixed small errors in dataset generation script; added script to add extra tokens with average embedding; created more useful description for tokens to get good init; made a package out of LLM_finetuner

LLM_finetuner/utils.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+from typing import Dict
 import torch
 from transformers.trainer_utils import get_last_checkpoint
 from transformers import AutoTokenizer, AutoModelForCausalLM
@@ -59,6 +60,42 @@ def set_pad_token_if_not_set(model, tokenizer):
         
     logger.info(f"Using padding token '{tokenizer.pad_token}'")
 
+def add_new_tokens_with_average_init(model, tokenizer, def_to_desc: Dict[str, str]):
+    """
+    For each (key, value) of def_to_desc, add "[key]" as a new token and initialize to average 
+    embedding of tokens in value
+    
+    Only initializes if the tokens are not already present. If only some are present, it raises an error.
+    
+    Skips None's in values
+    """
+    input_embeddings = model.get_input_embeddings()
+    output_embeddings = model.get_output_embeddings()
+    prev_num_tokens = len(tokenizer)
+
+    tokenizer.add_tokens([f"[{defn}]" for defn in def_to_desc.keys()], special_tokens=True)
+    if len(tokenizer) != prev_num_tokens + len(def_to_desc):
+        assert len(tokenizer) == prev_num_tokens, \
+            f"some, but not all tokens are already part of the vocabulary, {len(tokenizer)} != {prev_num_tokens}"
+        logger.warning("Not initializing def tokens because they are already part of the vocabulary")
+        return
+
+    new_token, token_desc = list(def_to_desc.items())[0]
+
+    for (new_token, token_desc) in def_to_desc.items():
+        if token_desc is None: continue
+        avg_embedding = input_embeddings(
+            tokenizer(token_desc, return_tensors="pt")["input_ids"][0]
+        ).mean(dim=0)
+        
+        token_id = tokenizer.convert_tokens_to_ids(new_token)
+        input_embeddings.weight.data[token_id] = avg_embedding
+        assert torch.all(input_embeddings.weight.data[token_id] == output_embeddings.weight.data[token_id]), "no weight tying, why?" # may try to set output embeddings
+        
+    logger.info(f"Vocabulary size: {len(tokenizer)}")
+    model.resize_token_embeddings(len(tokenizer))
+    
+    
 def setup_logging():
     logging.basicConfig(
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
@@ -69,12 +106,36 @@ def setup_logging():
     
 def main():
 
-    from transformers import AutoTokenizer
+    def test1():
+        from transformers import AutoTokenizer
+        model_name_or_path = "gpt2"
+        tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=False)
+        print(tokenizer.eos_token)
+        print(tokenizer.pad_token)
+    
+    def test2():
+        from transformers import AutoTokenizer, AutoModelForCausalLM
+        model_name_or_path = "gpt2"
+        tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+        model = AutoModelForCausalLM.from_pretrained(model_name_or_path)
 
-    model_name_or_path = "gpt2"
-    tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=False)
-    print(tokenizer.eos_token)
-    print(tokenizer.pad_token)
+        import yaml
+        from pathlib import Path
+        import os
+        # for creating this file
+        # patterns_filename = Path(os.path.expanduser("~/reinforcement/alphageometry/assets/def-patterns.yml"))
+        # print("\n".join(list(f"{x}: {x}" for x in yaml.safe_load(patterns_filename.read_text()).keys())))
+        patterns_desc_filename = Path(os.path.expanduser("~/reinforcement/alphageometry/assets/def-patterns-desc.yml"))
+
+        def_to_desc = yaml.safe_load(patterns_desc_filename.read_text())
+        print(f"Tok len: {len(tokenizer)}")    
+        add_new_tokens_with_average_init(model, tokenizer, def_to_desc)
+        print(f"Tok len: {len(tokenizer)}")
+        
+    print("Test1")
+    test1()
+    print("Test2")
+    test2()
     
 if __name__ == "__main__":
     main()