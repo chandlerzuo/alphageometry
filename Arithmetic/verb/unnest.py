199acaffeb59497ba1729f2ff1db1efab519b6e5
felixludos <felixludos@hotmail.com>
2024-05-30 21:58:49 +0200

    feat: Add Arithmetics class with unnest_expression method
    The code changes introduce a new `Arithmetics` class that includes the `unnest_expression` method. This method takes an expression as input and returns a list of operations. The operations are represented as tuples, with each tuple containing the variable name, function name, and arguments.
    
    Recent user commits:
    - Verbalization templates added
    - Renaming some funcs to capture some aspect of their function
    - unnest ready
    - copy geo verb
    
    Recent repository commits:
    - Merge branch 'main' of github.com:felixludos/alphageometry
    - Verbalization templates added
    - Renaming some funcs to capture some aspect of their function
    - unnest ready
    - copy geo verb
    - Merge pull request #1 from felixludos/nl_verbalization
    - Merge branch 'nl_verbalization'
    - Renaming
    - Now operations are classes. Interventional gen is functioning
    - Creating a new formal system based on Arithmetic

--- Arithmetic/verb/unnest.py
+++ Arithmetic/verb/unnest.py
@@ -1,49 +1,64 @@
-
+from typing import Optional, Union
 import ast
+from omniply import ToolKit, tool
 
-def unnest_expression(expression):
-	tree = ast.parse(expression, mode='eval')
 
-	# This list will hold all our operations
-	operations = []
-	# This counter will help us create unique variable names
-	var_counter = [0]
+class Arithmetics(ToolKit):
 
-	def handle_node(node):
-		if isinstance(node, ast.Num):  # For numbers
-			var_name = f'v{var_counter[0]}'
-			var_counter[0] += 1
-			operations.append((var_name, None, node.n))
-			return var_name
-		elif isinstance(node, ast.Name):  # For variables
-			var_name = f'v{var_counter[0]}'
-			var_counter[0] += 1
-			operations.append((var_name, None, node.id))
-			return var_name
-		elif isinstance(node, ast.Call):
-			args = [handle_node(arg) for arg in node.args]
-			func_name = node.func.id
-			var_name = f'v{var_counter[0]}'
-			var_counter[0] += 1
-			operations.append((var_name, func_name, args))
-			return var_name
-		else:
-			raise ValueError("Unsupported expression type")
+	@tool('operations')
+	@staticmethod
+	def unnest_expression(expression: str) -> list[Union[tuple[str, None, Union[str, int]], tuple[str, str, list[str]]]]:
+		tree = ast.parse(expression, mode='eval')
 
-	handle_node(tree.body)
+		# This list will hold all our operations
+		operations = []
+		# This counter will help us create unique variable names
+		var_counter = [0]
 
-	operations[-1] = (None, *operations[-1][1:])
+		def handle_node(node):
+			if isinstance(node, ast.Num):  # For numbers
+				var_name = f'v{var_counter[0]}'
+				var_counter[0] += 1
+				operations.append((var_name, None, node.n))
+				return var_name
+			elif isinstance(node, ast.Name):  # For variables
+				var_name = f'v{var_counter[0]}'
+				var_counter[0] += 1
+				operations.append((var_name, None, node.id))
+				return var_name
+			elif isinstance(node, ast.Call):
+				args = [handle_node(arg) for arg in node.args]
+				func_name = node.func.id
+				var_name = f'v{var_counter[0]}'
+				var_counter[0] += 1
+				operations.append((var_name, func_name, args))
+				return var_name
+			else:
+				raise ValueError("Unsupported expression type")
 
-	return operations
+		handle_node(tree.body)
+
+		operations[-1] = (None, *operations[-1][1:])
+
+		return operations
+
+
 
 def test_unnest():
-	result = unnest_expression("f(g(1), h(x))")
+	result = Arithmetics.unnest_expression("f(g(1), h(x))")
 	expected = [
-		('v0', None, 1), ('v1', 'g', ['v0']), ('v2', None, 'x'), ('v3', 'h', ['v2']), (None, 'f', ['v1', 'v3'])
+		('v0', None, 1),
+		('v1', 'g', ['v0']),
+		('v2', None, 'x'),
+		('v3', 'h', ['v2']),
+		(None, 'f', ['v1', 'v3'])
 	]
 	assert result == expected
 
-	result = unnest_expression('f(g(h(x), y))')
-	
+	result = Arithmetics.unnest_expression('f(g(h(x), y))')
+	expected = [
+
+	]
+
 
 
