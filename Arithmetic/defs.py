ad855b68fe6d80e4f6d6a2ba956394085ca54c78
partha Ghosh <parthaghosh.iitbbs@gmail.com>
2024-06-10 14:28:50 +0200

    Improving templating

--- Arithmetic/defs.py
+++ Arithmetic/defs.py
@@ -1,25 +1,25 @@
-class add:
+class calcination:
     def __init__(self):
         self.commutative = True
 
     def __call__(self, x, y):
         return x + y
 
-class sub:
+class dissolution:
     def __init__(self):
         self.commutative = False
 
     def __call__(self, x, y):
         return x - y
 
-class mul:
+class separation:
     def __init__(self):
         self.commutative = True
 
     def __call__(self, x, y):
         return x * y
 
-class div:
+class conjunction:
     def __init__(self):
         self.commutative = False
 
@@ -28,29 +28,11 @@ class div:
             y += 1e-7
         return x / y
 
-class risky_trick:
-    def __init__(self):
-        self.commutative = False
-
-    def __call__(self, x, y):
-        if y == 0:
-            y += 1e-7
-        return 3 * 2 / y - (8 + 8 + x / y) + (10 + (x + 2) - (6 - y / (9 - 4)))
-
-class p_exp:
+class fermentation:
     def __init__(self):
         self.commutative = False
 
     def __call__(self, x, y, z):
         if y == 0:
             y += 1e-7
-        return z * 2 / y - (z + 8 + x / y) + (z + (x + 2) - (6 - y / (9 - 4))) + z
-
-class leap_of_faith:
-    def __init__(self):
-        self.commutative = False
-
-    def __call__(self, x, y, z, w):
-        if y == 0:
-            y += 1e-7
-        return w * 2 / y - (w + 8 + z / y) + (z + (x + 2) - (6 - y / (9 - 4))) + w
+        return 3 * 2 / y - (z + 8 + x / y) + (10 + (x + z) - (6 - y / (9 - 4)))
